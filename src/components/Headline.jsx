efb5b3c00db30ff03111d5f3b09a3a491f0c3a24
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-07-20 15:51:44 -0300

    Requisito 5

--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 class Headline extends React.Component {
-    render() {
-        return (
-            <div>
-                <Title headline='Planetas' />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Title headline="Planetas" />
+      </div>
+    );
+  }
 }
 
 export default Headline;
--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 class PlanetCard extends React.Component {
-  render () {
+  render() {
     return (
       <div data-testid="planet-card">
         <p>{ this.props.planetName }</p>
-        <img src={ this.props.planetImage } alt='Planeta {planetName}' />
+        <img src={ this.props.planetImage } alt={ `Planeta ${this.props.planetName}` } />
       </div>
     );
   }
