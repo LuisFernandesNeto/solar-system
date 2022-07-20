585ca552226d02c8676d37aa6a59d26ada10f30a
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-07-20 16:40:27 -0300

    Requisito 6

--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Title from './Title';
 
 class Headline extends React.Component {
   render() {
--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class PlanetCard extends React.Component {
   render() {
+    const { planetName, planetImage } = this.props;
     return (
       <div data-testid="planet-card">
-        <p>{ this.props.planetName }</p>
-        <img src={ this.props.planetImage } alt={ `Planeta ${this.props.planetName}` } />
+        <p>{planetName}</p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
     );
   }
 }
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
 
 export default PlanetCard;
